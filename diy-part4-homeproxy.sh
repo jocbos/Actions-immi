aa56ad52c00daf1f073d0daa737fbc7c1b6f64c0
jocbos <9578116@qq.com>
2026-02-14 13:44:24 +0800

    Update and rename deepseek_bash_20260214_f3d874.sh to diy-part4-homeproxy.sh

--- diy-part4-homeproxy.sh
+++ diy-part4-homeproxy.sh
@@ -15,7 +15,7 @@ cd openwrt
 ./scripts/feeds install -a
 
 # 修改默认IP为 192.168.2.1 (避免与光猫冲突)
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # 修改默认主题为 argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
