fa6875203e5137ae478f0797eee18b8b0dda7210
jocbos <9578116@qq.com>
2026-02-15 11:19:55 +0800

    Update diy-part4-homeproxy.sh

--- diy-part4-homeproxy.sh
+++ diy-part4-homeproxy.sh
@@ -3,7 +3,6 @@
 
 # 添加额外的软件源（如果需要）
 cat >> feeds.conf.default <<EOF
-# src-git small8 https://github.com/kenzok8/small-package
 src-git kenzo https://github.com/kenzok8/openwrt-packages
 src-git small https://github.com/kenzok8/small
 EOF
