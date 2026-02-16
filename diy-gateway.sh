29e01649671aa10f61360873ae00e265b59e7be3
jocbos <9578116@qq.com>
2026-02-16 13:04:01 +0800

    Update diy-gateway.sh

--- diy-gateway.sh
+++ diy-gateway.sh
@@ -24,6 +24,8 @@ net.core.rmem_max = 134217728
 net.core.wmem_max = 134217728
 net.ipv4.tcp_rmem = 4096 87380 134217728
 net.ipv4.tcp_wmem = 4096 65536 134217728
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbr
 EOF
 
 # 默认防火墙规则
